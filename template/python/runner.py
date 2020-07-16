0838c751017f45aca7b11c4ddf5a378d4c95c972
Jaiwardhan Swarnakar <jaiwardhan_live@hotmail.com>
2020-07-16 20:22:35 +0000

    [Dbg] Added debugging support with time summary

--- template/python/runner.py
+++ template/python/runner.py
@@ -3,6 +3,7 @@
 
 from code import Solution
 import json
+import time
 
 class TestDriver:
     def __init__(self):
@@ -12,12 +13,23 @@ class TestDriver:
         self.total_pass = 0
         self.total_fail = 0
         self.total_notrun = 0
+        self.time_taken = []
         with open("./test.json") as f:
             self.test_file = json.loads(f.read())
             f.close()
     
-    def test_pass(self, test_number):
-        print ( " [_/] Test case passed: %d" % (int(test_number)))
+    def evaluate_time_ms(self, start_time, end_time):
+        dif = end_time - start_time
+        dif = dif*1000000 #convert s to ms
+        return dif
+
+    def test_pass(self, test_number, start_time = None, end_time = None):
+        if start_time is None or end_time is None:
+            print ( " [_/] Test case passed: %d" % (int(test_number)))
+        else:
+            tt = self.evaluate_time_ms(start_time, end_time)
+            print ( " [_/] Test case passed: %d, Time taken: %4.2f ms" % (int(test_number), tt))
+            self.time_taken.append(tt)
         self.total_pass += 1
 
     def test_fail(self, test_number, expected, found):
@@ -36,16 +48,48 @@ class TestDriver:
                 self.total_fail,
                 str(self.total_pass*100 / self.total_cases) +"%"
             ))
+        if len(self.time_taken) > 0:
+            sum = 0.0
+            for i in self.time_taken:
+                sum += i
+            print ( "> Avg time taken %4.2f ms" % (sum/len(self.time_taken)))
+            print ( "> Total time taken %4.2f ms" % (sum))
 
     def load_tests(self):
         print ( "Loading tests.." )
         self.tests = self.test_file["tests"]
         self.test_desc = self.test_file["description"]
         self.total_cases = len(self.tests)
+        self.to_debug = self.test_file["debug_tests"] if "debug_tests" in self.test_file else []
     
     def test(self):
         print ( "Starting tests.." )
-        
+        s = Solution()
+        to_test = self.tests
+        if len(self.to_debug) > 0:
+            to_test = []
+            for to_debug_test_nums in self.to_debug:
+                to_test.append(self.tests[to_debug_test_nums])
+            print ("###################################################")
+            print ("[!] Would be debugging the following tests: ")
+            print (self.to_debug)
+            print ("###################################################")
+            print ("")
+
+        for i,t in enumerate(to_test):
+            time_start = time.time()
+            ##### your execution here #####
+
+
+            ###############################
+            time_end = time.time()
+
+            # if test passes
+            # self.test_pass(i, time_start, time_end)
+
+            # if test fails
+            # self.test_fail(i, expected, found, time_start, time_end)
+        self.test_verdict()
 
 t = TestDriver()
 t.load_tests()
