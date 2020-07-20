cbd9808bb811925418227ab5a5672bb509885d62
Jaiwardhan Swarnakar <jaiwardhan_live@hotmail.com>
2020-07-20 20:29:40 +0000

    [pass] added soln for easy/remove-duplicates-from-sorted-list

---
+++ easy/remove-duplicates-from-sorted-list/python/code.py
@@ -0,0 +1,47 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    
+    @staticmethod
+    def parse_into_node(some_list):
+        head = None
+        last = None
+        for k in some_list:
+            if head is None:
+                head = ListNode(k, None)
+                last = head
+                continue
+            last.next = ListNode(k, None)
+            last = last.next
+        return head
+
+    @staticmethod
+    def parse_from_node(some_node):
+        res = []
+        while some_node is not None:
+            res.append(some_node.val)
+            some_node = some_node.next
+        return res
+
+    @staticmethod
+    def list_matches_node(some_list, some_node):
+        i = 0
+        while i < len(some_list) and some_node is not None:
+            if some_list[i] != some_node.val:
+                return False
+            i += 1
+            some_node = some_node.next
+        return i == len(some_list) and some_node is None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        if head is None:
+            return None
+        last = head
+        while last is not None and last.next is not None:
+            if last.next.val == last.val:
+                # duplicate
+                last.next = last.next.next
+            last = last.next
+        return head
